72e08b3cccae59126ac407ca4a758b92019b9679
Harshit Singh <https://github.com/harshitsingh85420>
2021-07-09 16:44:22 +0530

    PROJECT dONE

---
+++ JS PROJECT/react-website-1/react-website-yt-1/src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Cards from '../Cards'
+import Footer from '../Footer'
+import '../../App.css'
+import HeroSection from '../HeroSection'
+function Home() {
+    return (
+        <>
+        <HeroSection/> 
+        <Cards/>   
+        <Footer/>   
+            </>
+    )
+}
+
+export default Home
