b1cc0f8a51b2684cf5ffcfcee2cdca0d86fce196
Harshit Singh <https://github.com/harshitsingh85420>
2021-07-10 14:07:54 +0530

    some projects

---
+++ JS PROJECT/todos-list/src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is an about component
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum minus voluptatem sed qui, vitae unde nemo corporis error, nihil quia possimus sapiente molestiae, reiciendis laudantium aliquam tenetur aliquid molestias porro ut modi soluta? Commodi.
+            </p>
+        </div>
+    )
+}
