b19077734479876945f893b28e208b26b7663671
Harshit Singh <harshitsingh85420@gmail.com>
2021-07-16 09:58:51 +0530

    soon will complete

--- JS PROJECT/hatflix/src/Row.js
+++ JS PROJECT/hatflix/src/Row.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import axios from './axios';
-import { useState ,useEffect} from 'react';
-function Row({title}) {
- const [movies, setMovies] = useState([]);
- useEffect(() => {
-     async function fetchData(){
-         const request=await axios.get(fetchUrl);
-         console.log(request);
-         return request;
-     }
-     fetchData();
- }, [movies]);
+import { useState, useEffect } from 'react';
+
+import './Row.css';
+const base_url = "http://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow }) {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+    console.table(movies);
     return (
         <div>
-            <h2>{title}</h2>
+            <div className="row">
+                <h3>{title}</h3>
+
+                <div className="row__posters">{
+                    movies.map(movie => (
+                        <img src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            key={movie.id} alt={movies.name} className={`row__poster ${isLargeRow && "row__posterLarge"}`} />
+                    ))
+                }
+
+                </div>
+            </div>
         </div>
     )
 }
